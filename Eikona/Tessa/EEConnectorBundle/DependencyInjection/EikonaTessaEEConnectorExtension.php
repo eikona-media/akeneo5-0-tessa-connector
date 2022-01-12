378002826aac68647844307547a80113819a1661
Timo Müller <t.mueller@eikona-media.de>
2022-01-12 09:44:57 +0100

    EIMEDAKMOD-217 Produktbild wird nicht mehr angezeigt

---
+++ Eikona/Tessa/EEConnectorBundle/DependencyInjection/EikonaTessaEEConnectorExtension.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Eikona\Tessa\EEConnectorBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class EikonaTessaEEConnectorExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+    }
+}
